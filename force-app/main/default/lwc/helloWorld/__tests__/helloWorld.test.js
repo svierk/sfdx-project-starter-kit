c72ba7ea3cc2c1d6dcac29e74ce6bdc379512837
Sebastiano Vierk <sebastiano.vierk@capgemini.com>
2022-01-23 15:07:02 +0100

    chore(setup): enhance eslint rules

--- force-app/main/default/lwc/helloWorld/__tests__/helloWorld.test.js
+++ force-app/main/default/lwc/helloWorld/__tests__/helloWorld.test.js
@@ -3,23 +3,22 @@ import { createElement } from 'lwc';
 
 describe('c-hello-world', () => {
   afterEach(() => {
-    // The jsdom instance is shared across test cases in a single file so reset the DOM
     while (document.body.firstChild) {
       document.body.removeChild(document.body.firstChild);
     }
   });
 
   it('displays greeting', () => {
-    // Create element
+    // given
     const element = createElement('c-hello-world', {
       is: HelloWorld
     });
     document.body.appendChild(element);
 
-    // Set public name property
+    // when
     element.name = 'World';
 
-    // Verify displayed greeting
+    // then
     return Promise.resolve().then(() => {
       const div = element.shadowRoot.querySelector('div');
       expect(div.textContent).toBe('Hello, World!');
