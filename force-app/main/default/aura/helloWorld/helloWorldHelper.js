2a8c41ca361c53e5762f8a09a657fa0a916cfe59
Sebastiano Vierk <s.vierk@hotmail.de>
2022-04-29 18:51:17 +0200

    refactor(setup): remove aura dependencies

--- force-app/main/default/aura/helloWorld/helloWorldHelper.js
+++
@@ -1,6 +0,0 @@
-({
-  helperMethod: function () {
-    // eslint-disable-next-line no-console
-    console.log('helper method code goes here');
-  }
-});
--- force-app/main/default/aura/helloWorld/helloWorldRenderer.js
+++
@@ -1,7 +0,0 @@
-({
-  // Your renderer method overrides go here
-  render: function () {
-    // do custom rendering here
-    return this.superRender();
-  }
-});
