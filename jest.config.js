e870d2c9e0250fe914e72d5f9b77c691978624f5
Sebastiano Vierk <s.vierk@hotmail.de>
2022-05-15 19:17:21 +0200

    chore(jest): add cobertura and junit reporter

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,16 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
 module.exports = {
   ...jestConfig,
-  modulePathIgnorePatterns: ['/.localdevserver']
+  coverageReporters: ['clover', 'json', 'text', 'lcov', 'cobertura'],
+  modulePathIgnorePatterns: ['/.localdevserver'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'tests',
+        outputName: 'test-results-lwc.xml'
+      }
+    ]
+  ]
 };
